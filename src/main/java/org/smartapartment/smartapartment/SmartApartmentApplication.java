99ad6564d1929ae512158ffdb54617f4851ddbcc
gjx666 <3241073730@qq.com>
2025-11-24 14:31:48 +0800

    first commit

---
+++ src/main/java/org/smartapartment/smartapartment/SmartApartmentApplication.java
@@ -0,0 +1,13 @@
+package org.smartapartment.smartapartment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartApartmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartApartmentApplication.class, args);
+	}
+
+}
